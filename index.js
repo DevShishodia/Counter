d6e8ee641f6238d301d9a7d7df5d3ee847715d92
DevShishodia <dshishodia56@gmai.com>
2021-12-05 07:44:11 -0800

    Counter

---
+++ index.js
@@ -0,0 +1,13 @@
+let i=0;
+function increase(){
+    let e = document.getElementById("increase");
+   
+    e.innerText = ++i;
+
+}
+function decrease(){
+    let f = document.getElementById("increase");
+   
+    f.innerText = --i;
+  
+}
